f2e206b7f8a9018e65095bde6f3bc91ef869bc6a
hongjeonggi <hongjeonggi35@gmail.com>
2022-02-11 15:43:07 +0900

    Add Basic UI

--- Salary/Salary/ViewController.swift
+++ Salary/Salary/ViewController.swift
@@ -8,16 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var mondayButton: UIButton!
+    @IBOutlet weak var tuesdayButton: UIButton!
+    @IBOutlet weak var wednesdayButton: UIButton!
+    @IBOutlet weak var thursdayButton: UIButton!
+    @IBOutlet weak var fridayButton: UIButton!
+    @IBOutlet weak var saturdayButton: UIButton!
+    @IBOutlet weak var sundayButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
- 
+        scrollView.delegate = self
+        
     }
     
-
-
+    @IBAction func tabMonButton(_ sender: UIButton) {
+        print("mon")
+        
+        if mondayButton.isSelected {
+            mondayButton.isSelected = false
+        } else {
+            mondayButton.isSelected = true
+        }
+    }
+    
+  
+    
 }
 
+// MARK: ScrollViewDelegate
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        print("\(velocity)")
+    }
+}
